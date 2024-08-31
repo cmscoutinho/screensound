08245c728fda854c6fa346d5554e0a7171a5e23e
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-08-30 22:48:45 -0300

    feat: create main menu

--- src/main/java/br/com/coutsoft/screensound/ScreensoundApplication.java
+++ src/main/java/br/com/coutsoft/screensound/ScreensoundApplication.java
@@ -1,5 +1,6 @@
 package br.com.coutsoft.screensound;
 
+import br.com.coutsoft.screensound.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ public class ScreensoundApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hi!");
+		Main main = new Main();
+		main.showMenu();
 	}
 }
--- src/main/java/br/com/coutsoft/screensound/main/Main.java
+++ src/main/java/br/com/coutsoft/screensound/main/Main.java
@@ -2,4 +2,16 @@ package br.com.coutsoft.screensound.main;
 
 public class Main {
 
+    public void showMenu() {
+        System.out.println("""
+                Welcome to Screensound!
+                Choose an option:
+                
+                1-Register artist
+                2-Register song
+                3-List songs
+                4-Search song by artist
+                5-Look for info about artist
+                9-Exit""");
+    }
 }
