2dcf4d6ca34a16040d96d3170a8f00da8ea73ca1
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-08-30 22:51:16 -0300

    feat: processe user's option

--- src/main/java/br/com/coutsoft/screensound/main/Main.java
+++ src/main/java/br/com/coutsoft/screensound/main/Main.java
@@ -1,17 +1,39 @@
 package br.com.coutsoft.screensound.main;
 
+import java.util.Scanner;
+
 public class Main {
 
     public void showMenu() {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("""
                 Welcome to Screensound!
                 Choose an option:
-                
+                                
                 1-Register artist
                 2-Register song
                 3-List songs
                 4-Search song by artist
                 5-Look for info about artist
                 9-Exit""");
+
+        var option = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (option) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 9:
+                break;
+        }
     }
 }
