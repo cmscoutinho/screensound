d395762919a925fc653151d8e082c99affecf0bd
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-03 22:48:28 -0300

    feat: create JPA repository

---
+++ src/main/java/br/com/coutsoft/screensound/repository/ArtistRepository.java
@@ -0,0 +1,6 @@
+package br.com.coutsoft.screensound.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
+}
