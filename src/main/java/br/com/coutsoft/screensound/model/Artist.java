8cd8a958db4228d829aee574a1935c9e9d7d2c26
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-04 21:10:44 -0300

    feat: create Artist entity attributes and methods

--- src/main/java/br/com/coutsoft/screensound/model/Artist.java
+++ src/main/java/br/com/coutsoft/screensound/model/Artist.java
@@ -1,4 +1,42 @@
 package br.com.coutsoft.screensound.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Artist {
+    @Id
+    private Integer id;
+    private String name;
+    private String type;
+
+    public Artist() {
+
+    }
+
+    public Artist(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " | " + this.type;
+    }
 }
--- src/main/java/br/com/coutsoft/screensound/repository/ArtistRepository.java
+++ src/main/java/br/com/coutsoft/screensound/repository/ArtistRepository.java
@@ -1,5 +1,6 @@
 package br.com.coutsoft.screensound.repository;
 
+import br.com.coutsoft.screensound.model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
