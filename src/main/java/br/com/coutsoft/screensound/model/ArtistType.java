2d99f0473d73f44fb0e82a06084b76753f491edf
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-10 11:05:41 -0300

    feat: create fields and initialization for enum ArtistType

--- src/main/java/br/com/coutsoft/screensound/model/ArtistType.java
+++ src/main/java/br/com/coutsoft/screensound/model/ArtistType.java
@@ -1,4 +1,14 @@
 package br.com.coutsoft.screensound.model;
 
 public enum ArtistType {
+
+    SOLO("solo"),
+    DUO("duo"),
+    BAND("band");
+
+    private String artistType;
+
+    ArtistType(String artistType) {
+        this.artistType = artistType;
+    }
 }
