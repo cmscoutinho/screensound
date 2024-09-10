3ab0e438898f58c595335092f5a45bdac24e7cbf
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-10 11:07:51 -0300

    feat: create ArtistType.fromString()

--- src/main/java/br/com/coutsoft/screensound/model/ArtistType.java
+++ src/main/java/br/com/coutsoft/screensound/model/ArtistType.java
@@ -2,13 +2,21 @@ package br.com.coutsoft.screensound.model;
 
 public enum ArtistType {
 
-    SOLO("solo"),
-    DUO("duo"),
-    BAND("band");
+    SOLO("solo"), DUO("duo"), BAND("band");
 
     private String artistType;
 
     ArtistType(String artistType) {
         this.artistType = artistType;
+    }
+
+    public static ArtistType fromString(String artistType) {
+        for (ArtistType type : ArtistType.values()) {
+            if (type.artistType.equalsIgnoreCase(artistType)) {
+                return type;
+            }
+        }
+
+        throw new EnumConstantNotPresentException(ArtistType.class, "Artist type not found!");
     }
 }
