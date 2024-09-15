f642406e425b4184f01da7b57034d6619a7c403f
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-15 11:48:31 -0300

    refactor: configure text from response JSON on CohereMessage

--- src/main/java/br/com/coutsoft/screensound/model/CohereMessage.java
+++ src/main/java/br/com/coutsoft/screensound/model/CohereMessage.java
@@ -1,5 +1,9 @@
 package br.com.coutsoft.screensound.model;
 
 
-public record CohereMessage() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record CohereMessage(@JsonAlias("text") String text) {
 }
