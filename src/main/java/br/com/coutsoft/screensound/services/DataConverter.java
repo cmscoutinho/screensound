a5846b452e1eee212ea6aef9e72eadcbd0d7e8c7
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-15 11:56:09 -0300

    feat: implement DataConverter.covertJson()

--- src/main/java/br/com/coutsoft/screensound/services/DataConverter.java
+++ src/main/java/br/com/coutsoft/screensound/services/DataConverter.java
@@ -1,4 +1,18 @@
 package br.com.coutsoft.screensound.services;
 
-public class DataConverter {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class DataConverter implements  IDataConverter{
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T convertJson(String json, Class<T> outClass) {
+        try {
+            return mapper.readValue(json, outClass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
