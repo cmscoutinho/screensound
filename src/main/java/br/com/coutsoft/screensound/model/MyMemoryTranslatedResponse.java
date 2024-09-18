a3301b310cfb75292a86b73040d9b2b69c085c07
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-18 19:22:06 -0300

    refactor: map MyMemoryTranslatedResponse field response

--- src/main/java/br/com/coutsoft/screensound/model/MyMemoryTranslatedResponse.java
+++ src/main/java/br/com/coutsoft/screensound/model/MyMemoryTranslatedResponse.java
@@ -4,5 +4,5 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record MyMemoryTranslatedResponse(@JsonAlias("translatedText") MyMemoryResponse response) {
+public record MyMemoryTranslatedResponse(@JsonAlias("response") MyMemoryResponse response) {
 }
