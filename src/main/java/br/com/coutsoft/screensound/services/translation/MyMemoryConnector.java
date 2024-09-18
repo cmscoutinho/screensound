8746d52d2082746275f771854e6cf531ef6947f7
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-18 19:11:31 -0300

    feat: create method MyMemoryConnector.consume()

--- src/main/java/br/com/coutsoft/screensound/services/translation/MyMemoryConnector.java
+++ src/main/java/br/com/coutsoft/screensound/services/translation/MyMemoryConnector.java
@@ -1,4 +1,30 @@
 package br.com.coutsoft.screensound.services.translation;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class MyMemoryConnector {
+    private final String BASE_URL = "https://api.mymemory.translated.net/get?q=";
+    private final String LANG_PAIR = "en|pt-br";
+
+    public String consume(String prompt) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + URLEncoder.encode(prompt) + "&langpair=" + LANG_PAIR))
+                .build();
+
+        String json = "";
+        try {
+            HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            json = response.body().toString();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return json;
+    }
 }
