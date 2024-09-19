9da9098dc909ed026a17629af5058a6354c3d50b
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-19 19:10:47 -0300

    feat: implement MyMemoryAPIQuery.translate()

--- src/main/java/br/com/coutsoft/screensound/services/translation/MyMemoryAPIQuery.java
+++ src/main/java/br/com/coutsoft/screensound/services/translation/MyMemoryAPIQuery.java
@@ -1,4 +1,26 @@
 package br.com.coutsoft.screensound.services.translation;
 
+import br.com.coutsoft.screensound.model.MyMemoryTranslatedResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class MyMemoryAPIQuery {
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    public String translate(String input) {
+        MyMemoryAPIConsumer consumer = new MyMemoryAPIConsumer();
+
+        String json = consumer.consume(input);
+
+        MyMemoryTranslatedResponse response;
+
+        try {
+            response = mapper.readValue(json, MyMemoryTranslatedResponse.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return response.response().translatedText();
+    }
 }
