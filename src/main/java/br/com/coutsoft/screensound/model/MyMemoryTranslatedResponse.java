8b352ab7d7154ddcc71fbd824ffc7f758c6cf137
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-19 20:04:57 -0300

    refactor: rename MyMemoryTranslatedResponse.response to responseData

--- src/main/java/br/com/coutsoft/screensound/model/MyMemoryTranslatedResponse.java
+++ src/main/java/br/com/coutsoft/screensound/model/MyMemoryTranslatedResponse.java
@@ -4,5 +4,5 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record MyMemoryTranslatedResponse(@JsonAlias("response") MyMemoryResponse response) {
+public record MyMemoryTranslatedResponse(@JsonAlias("responseData") MyMemoryResponse responseData) {
 }
